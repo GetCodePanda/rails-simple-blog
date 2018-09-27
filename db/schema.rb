446903915f36033046dc370c7d1626cad4d088bb
YOUR NAME <YOUR@EMAIL.com>
2018-09-27 18:45:37 +0530

    completed blog without auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_094630) do
+ActiveRecord::Schema.define(version: 2018_09_27_121615) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
